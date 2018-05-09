983a00339c4577f3fde8c5f75e238b670894cab6
TrishulManna <trishul_@hotmail.com>
2018-05-09 09:57:29 +0200

    Test 1
    Robot

---
+++ RobotX/roboback/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use App\Project;
+use App\Role;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('moderator-only', function (){
+            if (\App\RoleUsers::where('user_id', Auth::id())->first()->role_id <= 2){
+                return true;
+            }else{
+                return false;
+            }
+        });
+    }
+}
